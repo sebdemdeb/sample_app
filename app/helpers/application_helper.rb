b179aae2f29bc9b16c0ceebcf0efa2c8acddbc11
Sébastien Demange <seb@seb-sb85-debian.(none)>
2012-12-29 03:01:44 +0100

    fin exercice layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-
+	
+	def logo
+		logo = image_tag("logo.png", :alt => "Application exemple", :class => "round")
+	end
+	
   # Retourner un titre basé sur la page.
   def titre
     base_titre = "Simple App du Tutoriel Ruby on Rails"
@@ -9,4 +13,5 @@ module ApplicationHelper
       "#{base_titre} | #{@titre}"
     end
   end
+  
 end
